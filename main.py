ee36c8fd632394ba27e169e0a24e7d5f41166743
Winsor Jeremiah <158396519+walpuerto@users.noreply.github.com>
2025-03-18 21:01:29 +0800

    Added Z from Probability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Z-score to Approximate Probability
-# Version 0.2
+# Version 0.3
 # By noob_undone
 
 def isValidFloat(value):
@@ -7,17 +7,24 @@ def isValidFloat(value):
     except: return False
     else: return True
 
-def updateText(input):
+def updateTextZ(input):
     if isValidFloat(input):
         zScore = input
         t4.config(text=f"Probability of Z-score {zScore} is: {precomputed.getProbabilityFromZ(zScore)}")
     else:
         t4.config(text=f"{input} is not a valid input.")
 
+def updateTextP(input):
+    if isValidFloat(input):
+        probability = input
+        t6.config(text=f"Z-score of Probability {probability} is: {precomputed.getZFromProbability(probability)}")
+    else:
+        t6.config(text=f"{input} is not a valid input.")
+
 import precomputed
 import tkinter
 m = tkinter.Tk()
-m.geometry("400x300")
+m.geometry("400x500")
 m.title('Z-score to Approximate Probability')
 
 # Prepare GUI
@@ -30,19 +37,41 @@ t2.pack()
 spacer1 = tkinter.Label(m, text="")
 spacer1.pack(pady=15)
 
+# ZScore Input
+
 t3 = tkinter.Label(m, text="Enter Z-score", font=("Calibri", 12))
 t3.pack()
 
 e1 = tkinter.Entry(m, width=30, font=("Calibri", 12))
 e1.pack()
 
-b1 = tkinter.Button(m, text="Calculate", font=("Calibri", 12), command=lambda: updateText(e1.get()))
+b1 = tkinter.Button(m, text="Calculate", font=("Calibri", 12), command=lambda: updateTextZ(e1.get()))
 b1.pack(pady=5)
 
-spacer1 = tkinter.Label(m, text="")
-spacer1.pack(pady=3)
+spacer2 = tkinter.Label(m, text="")
+spacer2.pack(pady=3)
 
-t4 = tkinter.Label(m, text="", font=("Calibri", 12))
+t4 = tkinter.Label(m, text="", font=("Calibri", 12), fg="red")
 t4.pack()
+
+spacer3 = tkinter.Label(m, text="")
+spacer3.pack(pady=15)
+
+# Probability Input
+
+t5 = tkinter.Label(m, text="Enter Probability", font=("Calibri", 12))
+t5.pack()
+
+e2 = tkinter.Entry(m, width=30, font=("Calibri", 12))
+e2.pack()
+
+b2 = tkinter.Button(m, text="Calculate", font=("Calibri", 12), command=lambda: updateTextP(e2.get()))
+b2.pack(pady=5)
+
+spacer4 = tkinter.Label(m, text="")
+spacer4.pack(pady=3)
+
+t6 = tkinter.Label(m, text="", font=("Calibri", 12), fg="red")
+t6.pack()
 
 m.mainloop()
